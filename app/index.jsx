77e6b8de36fec576390b1042335b52f8efcb60d1
zygimantas <zygimantasdev@gmail.com>
2016-04-21 11:05:32 +0300

    moving to redux

--- app/index.jsx
+++ app/index.jsx
@@ -3,12 +3,20 @@ import './main.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
-
+import { Provider } from 'react-redux';
+import { configureStore } from './store/configureStore';
+import { kanban } from './reducers';
 main();
 
 function main() {
+    let store = createStore();
+
     const app = document.createElement('div');
     document.body.appendChild(app);
-    ReactDOM.render(<App />, app);
+    ReactDOM.render(
+        <Provider store={}>
+            <App />
+        </Provider>, app
+    );
 }
 
