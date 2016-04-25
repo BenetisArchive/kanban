f25577e9b9c605b57f45cb291cc47a07bc185a6f
zygimantas <zygimantasdev@gmail.com>
2016-04-25 18:35:07 +0300

    reducer now acts upon attaching note to the list

--- app/reducers/lists.js
+++ app/reducers/lists.js
@@ -11,6 +11,29 @@ export default function lists(state = initialState, action) {
         case types.DELETE_LIST:
             return state.filter((list) => list.id !== action.id);
 
+        case types.ATTACH_TO_LIST:
+            const laneId = action.listId;
+            const noteId = action.noteId;
+
+            return state.map((list) => {
+                const index = list.notes.indexOf(noteId);
+
+                if(index >= 0) {
+                    return Object.assign({}, list, {
+                        notes: list.notes.length > 1 ? list.notes.slice(0, index).concat(
+                            list.notes.slice(index + 1)
+                        ): []
+                    });
+                }
+                if(list.id === laneId) {
+                    return Object.assign({}, list, {
+                        notes: [...list.notes, noteId]
+                    });
+                }
+
+                return list;
+            });
+
         default:
             return state;
     }
